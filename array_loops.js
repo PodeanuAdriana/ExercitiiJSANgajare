abb9fa1cee3f5aa22ebf8462ebca42ea97f9a43e
PodeanuAdriana <103220004+PodeanuAdriana@users.noreply.github.com>
2026-07-15 15:15:00 +0300

    add array elements

---
+++ array_loops.js
@@ -0,0 +1,9 @@
+// How to create a new array
+
+new Array(); // creates an empty array
+
+const fruits =["Banana", "Orange", "Apple", "Mango"]; // creates an array with 4 elements
+let fruit=fruits[2]; // accesses the first element of the array, which is "Banana"
+console.log(fruit); // outputs "Apple"
+v1=fruits.at("Orange"); // accesses the first element of the array, which is "Banana"
+console.log(v1); // outputs "Apple"
