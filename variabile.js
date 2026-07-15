458bc29e3a9a9c77e32d904f07d0d67272bad3b2
PodeanuAdriana <103220004+PodeanuAdriana@users.noreply.github.com>
2026-07-15 15:16:29 +0300

    ceva11

--- variabile.js
+++ variabile.js
@@ -3,8 +3,8 @@ let b=5;
 let z=a+b;
 let c=a*b;
 let ceva = 1;
-let ceva1 =" sa intamplat ceva "
-console.log(ceva)
+let ceva1 =" sa intamplat ceva ";
+console.log(ceva);
 let text = (age < 18) ? "Minor" : "Adult";
 console.log(text);
 let text1 = (age > 18) ? "Adult" : "Minor";
